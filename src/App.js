290b5cac659240ed656fbcd838727195a443f34d
David Uebelacker <david@uebelacker.ch>
2025-03-20 12:03:37 +0100

    :tada: Q:	How many journalists does it take to screw in a lightbulb? A:	Three.  One to report it as an inspired government program to bring 	light to the people, one to report it as a diabolical government plot 	to deprive the poor of darkness, and one to win a Pulitzer prize for 	reporting that Electric Company hired a lightbulb-assassin to break 	the bulb in the first place.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+
+const queryParams = new URLSearchParams(window.location.search);
+const HUE_BRIDGE_IP = queryParams.get("hue-bridge-ip");
+const USERNAME = queryParams.get("username");
 
 function App() {
+  const [scenes, setScenes] = React.useState([]);
+  const [error, setError] = React.useState(null);
+
+  const loadScenes = async () => {
+    try {
+      const response = await fetch(
+        `http://${HUE_BRIDGE_IP}/api/${USERNAME}/scenes`,
+      );
+      const json = await response.json();
+      const scenes = Object.keys(json)
+        .map((key) => {
+          const scene = json[key];
+          return {
+            id: key,
+            name: scene.name,
+            group: scene.group,
+          };
+        })
+        .reverse();
+      setScenes(scenes);
+      console.log(scenes);
+    } catch (error) {
+      console.error("Error fetching scenes:", error);
+      setError("Only woks inside the nag network!");
+    }
+  };
+
+  const turnOff = async (index = 0) => {
+    try {
+      console.log(scenes);
+      console.log(scenes[0]);
+      const response = await fetch(
+        `http://${HUE_BRIDGE_IP}/api/${USERNAME}/groups/${scenes[index].group}/action`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ on: false }),
+        },
+      );
+      const json = await response.json();
+      console.log("Turned off:", json);
+
+      if (scenes.length > index + 1) {
+        turnOff(index + 1);
+      }
+    } catch (error) {
+      console.error("Error turning off:", error);
+    }
+  };
+
+  const turnOn = async (scene) => {
+    try {
+      const response = await fetch(
+        `http://${HUE_BRIDGE_IP}/api/${USERNAME}/groups/${scene.group}/action`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ scene: scene.id }),
+        },
+      );
+      const json = await response.json();
+      console.log("Turned on:", json);
+    } catch (error) {
+      console.error("Error turning on:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadScenes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>🚦 nag volume light</h1>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <div>
+          {scenes &&
+            scenes.map((scene) => (
+              <button onClick={() => turnOn(scene)}>{scene.name}</button>
+            ))}
+          <button onClick={() => turnOff()}>Fine</button>
+        </div>
+      )}
     </div>
   );
 }
